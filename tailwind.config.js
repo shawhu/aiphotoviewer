3e9f723bd99fbf27b4e5e59868fdd374c2235aeb
shawhu <shawhu@gmail.com>
2023-04-19 20:21:08 +0800

    added 16,20 24 columes css for grid auto control

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        16: "repeat(16, minmax(0, 1fr))",
+        20: "repeat(20, minmax(0, 1fr))",
+        24: "repeat(24, minmax(0, 1fr))",
+        28: "repeat(28, minmax(0, 1fr))",
       },
     },
   },
   plugins: [],
-}
+};
