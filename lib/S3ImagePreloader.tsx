821e271a4667b3e42582549196d6140cfb50db84
shawhu <shawhu@gmail.com>
2023-04-20 12:12:15 +0800

    remove literal s3 credential, use env instead

--- lib/S3ImagePreloader.tsx
+++ lib/S3ImagePreloader.tsx
@@ -1,14 +1,8 @@
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3"; // ES Modules import
 
 export async function listS3Bucket(shuffle: boolean = true) {
-  //credentials...
-  const creds = {
-    accessKeyId: "AKIA6JULPL6JZPFLHBWC",
-    secretAccessKey: "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",
-  };
-
   // a client can be shared by different commands.
-  const client = new S3Client({ region: "ap-northeast-1", credentials: creds });
+  const client = new S3Client({ region: "ap-northeast-1" });
   const params = {
     Bucket: "ai-tokyo-001", // required
   };
